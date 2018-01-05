a051bf11ca20bff9466cb0ef305620f6661a6cbb
qtxie <xqtxyz@gmail.com>
2018-01-05 11:53:51 +0800

    TEST: adds more tests.

--- test/token_test.js
+++ test/token_test.js
@@ -68,26 +68,39 @@ describe('Contract', function () {
             expect(await expectBalance(ren, INVESTOR3, toWei('3000')))
 
             // buying before presale will fail
-            //try {
-            //    await buy(web3, INVESTOR1, renCrowdfund, '1')
-            //} catch (error) {}
-            //await expectBalance(ren, INVESTOR1, toWei('1000'))
+            try {
+                await buy(web3, INVESTOR1, renCrowdfund, '1')
+            } catch (error) {}
+            await expectBalance(ren, INVESTOR1, toWei('1000'))
 
             // open presale
 			await send(renCrowdfund, DEPLOYER, 'openCrowdfund')
 
             // presale buy 1
+            // presale price: 1 ETH = 2750 REN
             await buy(web3, INVESTOR1, renCrowdfund, '1')
+            // 1000 + 2750 = 3750
             expect(await expectBalance(ren, INVESTOR1, toWei('3750')))
             expect(await web3.eth.getBalance(WALLET)).eq(toWei('101'))
             await buy(web3, INVESTOR1, renCrowdfund, '1')
+            // 3750 + 2750 = 6500
             expect(await expectBalance(ren, INVESTOR1, toWei('6500')))
             expect(await web3.eth.getBalance(WALLET)).eq(toWei('102')) 
 
             // presale buy 2
             await buy(web3, INVESTOR2, renCrowdfund, '2')
+            // 2000 + (2750 * 2) = 7500
             expect(await expectBalance(ren, INVESTOR2, toWei('7500')))
             expect(await web3.eth.getBalance(WALLET)).eq(toWei('104'))
+
+            // close presale
+            await send(ren, DEPLOYER, 'finalizePresale')
+
+            // Open round buy
+            await buy(web3, INVESTOR2, renCrowdfund, '1')
+            // 7500 + 2500 = 10,000
+            expect(await expectBalance(ren, INVESTOR2, toWei('10000')))
+            expect(await web3.eth.getBalance(WALLET)).eq(toWei('105'))
         })
     })
 
@@ -96,6 +109,41 @@ describe('Contract', function () {
             let symbol = (await ren.methods.symbol().call())
             expect(symbol).equal('REN')
         })
+
+        /* -- Crowd Fund Contract -- */
+        // Others except DEPLOYER call this function will fail
+        // changeWalletAddress
+
+        // Others except DEPLOYER call this function will fail
+        // openCrowdfund
+
+        // Others except DEPLOYER call this function will fail
+        // closeCrowdfund
+
+        /* -- Token Contract -- */
+        // Others except CrowdFund contract call this function will fail
+        // startCrowdfund
+
+        // Others except CrowdFund contract call this function will fail
+        // transferFromCrowdfund
+
+        // Others except CrowdFund contract call this function will fail
+        // finalizeCrowdfund
+
+        // Others except DEPLOYER call this function will fail
+        // setCrowdfundAddress
+
+        // Others except DEPLOYER call this function will fail
+        // releaseRedTeamTokens
+
+        // Others except DEPLOYER call this function will fail
+        // finalizePresale
+
+        // Others except DEPLOYER call this function will fail
+        // deliverPresaleRENaccounts
+
+        // Others except DEPLOYER call this function will fail
+        // changeRedTeamAddress
     })
 
     describe('ERC20 API Test', () => {
@@ -103,5 +151,15 @@ describe('Contract', function () {
             let symbol = (await ren.methods.symbol().call())
             expect(symbol).equal('REN')
         })
+
+        // transfer
+
+        // transferFrom
+
+        // approve
+
+        // allowance
+
+        // balanceOf
     })
 })
