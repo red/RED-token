d205ed870c0088e622ddbdc85ace2dce28031a28
Tamas Herman <hermantamas@gmail.com>
2018-01-08 01:27:00 +0800

    Display some token info on the web page

--- web/app.js
+++ web/app.js
@@ -1,4 +1,19 @@
 const L = console.log
+const $ = (sel) => document.querySelector(sel)
+const {toBN, fromWei, toWei} = web3
+
+const call = async (contract, methodName, ...params) =>{
+    const method = contract.methods[methodName]
+    if (method instanceof Function) {
+        return method(...params).call()
+    } else{
+        throw new Error(`${contract.options.name}.${methodName} is undefined`)
+    }
+}
+
+async function fetchJSON(url) {
+    return fetch(url, {headers: new Headers({'Content-Type': 'application/json'})})
+}
 
 class App {
     get red() {
@@ -13,15 +28,10 @@ class App {
         L('Constructing app...')
         this.web3 = new Web3(currentProvider)
         L('Web3 version:', this.web3.version)
-        this.loadContracts()
     }
 
-    async fetchJSON(url) {
-        return fetch(url, {headers: new Headers({'Content-Type': 'application/json'})})
-    }
-
-    async loadContract(interfaceJsonUrl) {
-        const {address, jsonInterface} = await (await this.fetchJSON(interfaceJsonUrl)).json()
+    async loadContract(jsonInterfaceUrl) {
+        const {address, jsonInterface} = await (await fetchJSON(jsonInterfaceUrl)).json()
         return new this.web3.eth.Contract(jsonInterface, address)
     }
 
@@ -35,7 +45,7 @@ class App {
         try {
             // If the token symbol is `bytes32` it can be converted to string like this:
             //   const sym = this.web3.utils.hexToUtf8(await this.red.methods.symbol().call())
-            const sym = await this.red.methods.symbol().call()
+            const sym = await call(this.red, 'symbol')
             L('Token symbol:', sym)
             return sym
         } catch (e) {
